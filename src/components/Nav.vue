8261f767a89a750dd9ffced708b24e3a5889dbe9
Jonathan <jsanch88@stu.southtexascollege.edu>
2021-11-18 23:47:04 -0600

    Edit to the name

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -2,7 +2,7 @@
   <div class="text-pink-200 bg-pink-800">
     <div class="container flex items-center justify-center mx-auto">
       <h1 class="py-8 text-3xl font-thin tracking-tighter">
-        Word<span class="font-normal">Dictionary</span>
+        Free<span class="font-normal">Advice</span>
       </h1>
     </div>
   </div>
