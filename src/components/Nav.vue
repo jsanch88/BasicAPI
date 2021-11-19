1e24292eb3d6dfc546c92e122bac3384701809bd
Jonathan <jsanch88@stu.southtexascollege.edu>
2021-11-18 22:29:08 -0600

    Adjusted creating the BasicAPI.

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -2,7 +2,7 @@
   <div class="text-pink-200 bg-pink-800">
     <div class="container flex items-center justify-center mx-auto">
       <h1 class="py-8 text-3xl font-thin tracking-tighter">
-        Advice<span class="font-normal">Call</span>
+        Word<span class="font-normal">Dictionary</span>
       </h1>
     </div>
   </div>
